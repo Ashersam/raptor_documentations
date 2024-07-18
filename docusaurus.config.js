c64fd8b4b719caa13a47cce164b0f5060ce66ccf
Ashersam <asher.samuel@raptorpos.com>
2024-07-18 14:11:27 +0530

    addedd new files

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -13,7 +13,7 @@ const config = {
   favicon: 'img/logo_webbo.png',
 
   // Set the production url of your site here
-  url: 'raptordocs.netlify.app',
+  url: 'https://raptordocs.netlify.app',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
   baseUrl: '/',
